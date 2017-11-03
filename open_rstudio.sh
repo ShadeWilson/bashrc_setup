e7620cd5328416c79064c1086b49e9f847c012e5
Shade Wilson <shadew@ihme.washington.edu>
2017-11-02 23:33:22 -0700

    Implement two-step url creation + sourcing

--- open_rstudio.sh
+++ open_rstudio.sh
@@ -3,6 +3,12 @@
 #### TODO: grep/awk for url base
 # grab port number (store)
 open_rstudio() {
-    start chrome www.google.com
+	PORT=6009 # find better way to grab default port
+	#base=`qstat | grep 'RSTUDIO' | awk '{a = $5 " " a} END {print a}'`
+	base=`echo "www.google.com"`
+	url=""$base":"$PORT""
+
+    start chrome "$url"
 }
 
+open_rstudio
