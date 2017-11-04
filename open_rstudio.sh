4e74020803e2127275465e533645f93ce0158640
Shade Wilson <shadew@ihme.washington.edu>
2017-11-03 21:54:36 -0700

    Add qrl to open_rstudio and other small fixes

--- open_rstudio.sh
+++ open_rstudio.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
+# grab the queue url (qrl) from qstat
+qrl() {
+    PORT=6095
+    base=`qstat | grep $USER | grep "rstudio" | awk '{a = $8 " " a} END {print a}' | sed -r 's#(^.*@)|(\s)##g'`
+    
+    url=""$base"gton.edu:"$PORT""
+    
+    echo "$url"
+}
 # This function automatically opens your session
 # of RStudio on the cluster that you've already started
 # with launch_rstudio. Can import a port number or set a default (recommended)
@@ -15,7 +24,7 @@ open_rstudio() {
 	fi
 	
 	# need the http:// here or else url wont be read properly
-	base=`echo "c2-6f16-16-2.ihme.washington.edu"`
+	base=`qrl`
 	url="http://"$base":"$PORT"/"
 	
 	echo "Launching RStudio at "$url"."
--- qrl.sh
+++ qrl.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-# function to read qstat and spit out url for RStudio
-qurl() {
+# function to grab the queue url (qrl) from qstat
+qrl() {
     PORT=6095
-    base=`qstat | grep $USER |  awk '{a = $8 " " a} END {print a}' | sed -r 's#(^.*@)|(\s)##g'`
+    base=`qstat | grep $USER | grep "rstudio" | awk '{a = $8 " " a} END {print a}' | sed -r 's#(^.*@)|(\s)##g'`
     
     url=""$base"gton.edu:"$PORT""
     
