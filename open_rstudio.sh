791a9b027ab352cec1d030f826fbb1f0c9f43a1a
Shade Wilson <shadew@ihme.washington.edu>
2017-11-03 17:01:46 -0700

    Add ability to enter post as argument

--- open_rstudio.sh
+++ open_rstudio.sh
@@ -3,12 +3,21 @@
 #### TODO: grep/awk for url base
 # grab port number (store)
 open_rstudio() {
-	PORT=6009 # find better way to grab default port
+	if [ -z "$1" ] 
+	then 
+		PORT=6095
+	else
+		 PORT="$1" # find better way to grab default port
+	fi
+	# c2-6f16-16-2.ihme.washington.edu:6095
+	
 	#base=`qstat | grep 'RSTUDIO' | awk '{a = $5 " " a} END {print a}'`
-	base=`echo "www.google.com"`
+	base=`echo "c2-6f16-16-2.ihme.washington.edu"`
 	url=""$base":"$PORT""
-
-    start chrome "$url"
+	echo $PORT
+	echo $url
+    #start chrome "$url"
+    start chrome youtube.com
 }
 
 open_rstudio
