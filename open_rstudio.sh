2d352bb2755767235d300323ad57e880fb58d6bf
Shade Wilson <shadew@ihme.washington.edu>
2017-11-03 21:21:41 -0700

    Fix url so open_rstudio works

--- open_rstudio.sh
+++ open_rstudio.sh
@@ -13,11 +13,11 @@ open_rstudio() {
 	
 	#base=`qstat | grep 'RSTUDIO' | awk '{a = $5 " " a} END {print a}'`
 	base=`echo "c2-6f16-16-2.ihme.washington.edu"`
-	url=""$base":"$PORT""
-	echo $PORT
-	echo $url
-    #start chrome "$url"
-    start chrome youtube.com
+	url="http://"$base":"$PORT"/"
+	
+	echo "Launching RStudio at "$url""
+    start chrome "$url" 
+    #start chrome youtube.com
 }
 
 open_rstudio
