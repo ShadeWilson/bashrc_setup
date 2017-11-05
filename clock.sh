71e98506321118077359c5419e04ba25938be89f
Shade Wilson <sw2ht@virginia.edu>
2017-11-04 22:31:58 -0700

    Put time components into array, create for in loop

--- clock.sh
+++ clock.sh
@@ -1,8 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 clock() {
     time=`date "+%I %M"`
     hour=`echo "$time" | awk '{a = $1 " " a} END {print a}'`
     min=`echo "$time" | awk '{a = $2 " " a} END {print a}'`
 
+    clock[1]="$hour"; clock[2]="$min"
+
+    for i in ${clock[@]}
+    do
+        echo "$i"
+    done
+
 }
+
+clock
