ba63a5968910c222c43d359cae851797e318033a
Shade Wilson <sw2ht@virginia.edu>
2017-11-05 12:10:38 -0800

    Add work on separating time into single digits

--- clock.sh
+++ clock.sh
@@ -8,6 +8,19 @@ clock() {
     hour=`echo "$time" | awk '{a = $1 " " a} END {print a}'`
     min=`echo "$time" | awk '{a = $2 " " a} END {print a}'`
 
+    # kinda a hacky way to grab the digits, but it works
+    hour2=`expr $hour % 10`
+    hour1=`expr $hour - $hour2 % 100 - 9`
+
+    time2=`expr $time % 10`
+    time1=`expr $time - $time2`
+    time1=`expr $time1 % 100`
+    time1=`expr $time1 / 10`
+
+
+    echo "Hour: "$hour1$hour2
+    echo "Min: "
+
     clock[1]="$hour"; clock[2]="$min"
 
     find numbers -regex '.*3.txt'
