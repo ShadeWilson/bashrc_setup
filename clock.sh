5f5ec9ced4d034f3927eea0c6f90ec04a5a74dc0
Shade Wilson <sw2ht@virginia.edu>
2017-11-05 11:07:00 -0800

    Starting beginning of working line additions

--- clock.sh
+++ clock.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 clock() {
+    echo > clock.output1
+    echo > clock.output2
+
     time=`date "+%I %M"`
     hour=`echo "$time" | awk '{a = $1 " " a} END {print a}'`
     min=`echo "$time" | awk '{a = $2 " " a} END {print a}'`
@@ -14,15 +17,22 @@ clock() {
 
     echo
 
-    range=8
+    range=2
     for ((i=0;i<="$range";i++))
     do
-        grep $i one.txt >> clock.output
-        grep $i two.txt >> clock.output
-        echo "for loop"
+        grep $i one.txt > clock.output1
+        grep $i two.txt >> clock.output1
+        #cat clock.output | sed -E 's#\n\d# #g' > clock.output
+        #sed 's#\n[0-9]# #'
+        line=`cat clock.output1 | tr '\n[0-9]'  ' ' `
+        line="$line"'\n'
+        echo "$line" >> clock.output2
     done
 
-    cat clock.output
+    cat clock.output2
+
+#rm clock.output1
+#rm clock.output2
 }
 
 
