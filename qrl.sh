03cc0554d67208f646c8112ff2f895abb7b663cd
Shade Wilson <shadew@ihme.washington.edu>
2017-11-07 11:21:31 -0800

    Fix regex bug

--- qrl.sh
+++ qrl.sh
@@ -4,7 +4,7 @@
 qrl() {
 if [ -z "$1" ] 
     then 
-        PORT=6095
+        PORT=6009
     else
          PORT="$1" # find better way to grab default port
     fi
@@ -12,11 +12,13 @@ if [ -z "$1" ]
     # grab job number
     # use job number to find exec_host_list aka queue name and take info we want
     JOB_NUM=`qstat | grep $USER | grep "rstudio" | awk '{a = $1 " " a} END {print a}'`
-    base=`qstat -j $JOB_NUM | grep "exec_host_list" | sed -r 's#(.*1:\s*)|(2\s*)##g'`
+    base=`qstat -j $JOB_NUM | grep "exec_host_list" | sed -r 's#(.*1:\s*)|(:2\s*)##g'`
 
-    url=""$base""$PORT""
+    url=""$base":"$PORT""
     
     echo "$url"
 }
 
 qurl
+
+###### Fun Functions #######
