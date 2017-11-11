d5419f12663f285ff23abcda3aec266ab32b1b7f
Shade Wilson <sw2ht@virginia.edu>
2017-11-10 16:57:50 -0800

    Comment code

--- up.sh
+++ up.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # function to go up a user-specified amount of directories
+# input number of directories you want to do up 
+# if you choose too high a number, it just puts you at the highest dir
+
+# needs to be in a .bashrc/.bash_profile file to work.
+# doesnt work as a shell script.
 function up() {
     if [ -z "$1" ] 
     then 
-        GO_UP=1
+        GO_UP=1 # default go up one
     else
-        GO_UP="$1" # find better way to grab default port
+        GO_UP="$1" 
     fi
 
     NEW_DIR=""
@@ -17,7 +22,7 @@ function up() {
     done
 
     cd $NEW_DIR
-    pwd
+    pwd # print final directory
 }
 
 up 2
