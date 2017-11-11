6d4f8507bbfafec098253547a79fa266bf67103f
Shade Wilson <sw2ht@virginia.edu>
2017-11-10 21:19:22 -0800

    Add different messages for each day of the week

--- greetings.sh
+++ greetings.sh
@@ -4,8 +4,19 @@ greetings() {
     
     date "+%r"
     date "+%B %d, %Y"
+    echo
 
     DAY=`date "+%A"`
+
+    case $DAY in
+        "Sunday") echo "Happy Sunday! Enjoy the rest of your weekend!" ;;
+        "Monday") echo "I know it's Monday, but you got this!" ;;
+	"Tuesday") echo "Happy Taco Tuesday!" ;;
+	"Wednesday") echo "HUMP DAAAAAAAAAAAAY!" ;;
+	"Thursday") echo "One more day until the weekend, hang in there!" ;;
+	"Friday") echo "Happy Friday!!" ;;
+	"Saturday") echo "Why are you on your computer on a Saturday...!" ;;
+    esac
 
 
 }
