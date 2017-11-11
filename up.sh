67cdff2c9007419e50b66a82e1242f0aad80e7c0
Shade Wilson <sw2ht@virginia.edu>
2017-11-10 16:42:01 -0800

    Add for loop for directory change

--- up.sh
+++ up.sh
@@ -8,7 +8,17 @@ up() {
         GO_UP="$1" # find better way to grab default port
     fi
 
-    echo $GO_UP
+    NEW_DIR=""
+
+    for i in `seq 1 $GO_UP`;
+    do 
+	NEW_DIR="$NEW_DIR"'../'
+    done
+
+    echo $NEW_DIR
+    cd $NEW_DIR
+   # cd $(printf "%0.0s../" $(seq 1 $1));
+    
 }
 
-up
+up 2
